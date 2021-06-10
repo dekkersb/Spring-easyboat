c110abd0fac8c3845c0268dcc28710d9607cedc8
dekkersbd <79840065+dekkersbd@users.noreply.github.com>
2021-06-10 15:25:43 +0200

    first commit

---
+++ src/main/java/com/example/easyboat/EasyBoatApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBoatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBoatApplication.class, args);
+    }
+
+}
